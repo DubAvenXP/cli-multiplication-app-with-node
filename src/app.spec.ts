ea05fd825b1883fc2fdfd9ef433258a1af256457
Alejandro Dubon <alejandrodubon88@gmail.com>
2023-11-01 16:35:07 -0600

    test: app.ts

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -1,5 +1,20 @@
-describe('app.ts', () => {
-  it('should be true', () => {
+import { ServerApp } from "./presentation/server-app";
 
+describe('app.ts', () => {
+  it('should be true', async () => {
+    const serverRunMock = jest.fn();
+    ServerApp.run = serverRunMock;
+
+    process.argv = ['node', 'app', '-b', '33', '-l', '22', '-s', '-d', 'test-destination', '-n', 'test-name'];
+
+    await import('./app');
+
+    expect(serverRunMock).toHaveBeenCalledWith({
+      base: 33,
+      limit: 22,
+      showTable: true,
+      fileDestination: 'test-destination',
+      fileName: 'test-name',
+    });
   });
 });
